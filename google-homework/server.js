50d10decb61eefff7c66e5f60b0b784d794fd657
Parker Manning <parkerlmanning@hotmail.com>
2021-01-05 20:41:38 -0600

    Added express routes to server page, need to go back and add in api routes

--- google-homework/server.js
+++ google-homework/server.js
@@ -4,3 +4,25 @@ const routes = require("./routes")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+//** need to go back and look these up and get a refresher **//
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// api / view routes
+app.use(routes);
+
+// Connect to the MongoDB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongo cluster url" {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+}
+);
+
+app.listen(PORT, () => console.log(`Started on ${PORT}`));
