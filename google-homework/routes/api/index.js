bc5190983b3d21c95ab2d891c513e69e5bacc2b2
Parker Manning <parkerlmanning@hotmail.com>
2021-01-05 20:21:16 -0600

    adding files for api routes

---
+++ google-homework/routes/api/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const router = require("express").Router();
+const bRoutes = require("./books");
+const gRoutes = require("./google");
+
+router.use("/books", bRoutes);
+router.use("/google", gRoutes);
+// for other routes rendering html
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+});
