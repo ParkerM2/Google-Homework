c0e0ce1ff705ecade11061e507a1b3283ce6fe91
Parker Manning <parkerlmanning@hotmail.com>
2021-01-05 21:47:07 -0600

    pushing added components

---
+++ google-homework/client/src/components/book/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Book({ title, subtitle, authors, link, description, image, Button }) {
+    return (
+        <div>
+            <h3>{title}</h3>
+            {subtitle && <h5>{subtitle}</h5>}
+            <p> Written by : {authors}</p>
+            <div>
+            <img src={image} alt={title} />
+            </div>
+            <div>
+            <a target="_blank" href={link}>
+                    view
+            </a>
+            <Button />
+            </div>
+            <div>
+            <p>{description}</p>
+            </div>
+        </div>
+    )
+}
