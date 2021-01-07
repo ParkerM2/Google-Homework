ec16748a8be07f07517321534cc649cd35e6f294
Parker Manning <parkerlmanning@hotmail.com>
2021-01-07 13:55:13 -0600

    heroku fix

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Parker:manningsigns1@cluster0.b3ah4.mongodb.net/googlebooks?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 //** need to go back and look these up and get a refresher **//
 app.use(express.urlencoded({ extended: true }));
@@ -35,16 +47,7 @@ app.use(routes);
 //   client.close();
 // });
 
-mongoose.connect(
-    process.env.MONGODB_URI ||
-    "mongodb://localhost/googlebooks",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-)
+
 // mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Parker:manningsigns1@cluster0.b3ah4.mongodb.net/googlebooks?retryWrites=true&w=majority");
 
 
